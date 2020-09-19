5a39cba73164a39e14974adb18587bd3acd45863
PookMook <arthur.juchereau@gmail.com>
2020-09-19 11:50:09 -0400

    base install + tsconfig

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from "components/button";
+export default function Main() {
+  return (
+    <>
+      <h1>Hello world</h1>
+      <Button />
+    </>
+  );
+}
